7ad294f866ba9a4bcf14eaa1811ae24e5825bec6
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-09-13 15:17:08 -0400

    Add variables related to "disable inactive users" Lambda

--- variables.tf
+++ variables.tf
@@ -1,4 +1,20 @@
 # ------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+#
+# You must provide a value for each of these parameters.
+# ------------------------------------------------------------------------------
+
+variable "cool_lambda_artifacts_s3_bucket" {
+  description = "The name of the bucket where COOL Lambda deployment packages are to be stored."
+  type        = string
+}
+
+variable "disable_inactive_users_lambda_key" {
+  description = "The S3 key associated with the Lambda function deployment package to disable inactive IAM users."
+  type        = string
+}
+
+# ------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 #
 # These parameters have reasonable defaults.
@@ -49,6 +65,18 @@ variable "provisionssmsessionmanager_policy_description" {
 variable "provisionssmsessionmanager_policy_name" {
   default     = "ProvisionSSMSessionManager"
   description = "The name to assign the IAM policy that allows sufficient permissions to provision the SSM Document resource and set up SSM session logging in the Cyber Hygiene account."
+  type        = string
+}
+
+variable "read_cool_lambda_bucket_policy_description" {
+  default     = "Allows read-only access to the bucket in the Terraform account containing Lambda deployments."
+  description = "The description to associate with the IAM role that allows read-only access to the bucket in the Terraform account containing Lambda deployments."
+  type        = string
+}
+
+variable "read_cool_lambda_bucket_policy_name" {
+  default     = "LambdaBucketReadOnly"
+  description = "The name to assign the IAM policy that allows read-only access to the bucket in the Terraform account containing Lambda deployments."
   type        = string
 }
 
